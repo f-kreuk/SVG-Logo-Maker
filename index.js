ab888fb80e0cae3362534940474b34dc933e9167
Florian Kreuk <fkreuk@cartermario.com>
2023-08-28 14:59:36 -0400

    Added prompt with questions for user input

--- index.js
+++ index.js
@@ -3,3 +3,34 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+//inquirer prompt/command line questions for user
+
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters of text to display in your logo.',
+      },
+      {
+        type: 'input',
+        name: 'textColor',
+        message: 'Select your text color by entering a color keyword or submitting a hexadecimal number.',
+      },
+      {
+        type: 'list',
+        name: 'shape',
+        message: 'Select the shape for your logo.',
+        choices: [
+            "Triangle",
+            "Square",
+            "Circle"
+        ]
+      },
+      {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Select your shape color by entering a color keyword or submitting a hexadecimal number.',
+      },
+    ]); 
+};
